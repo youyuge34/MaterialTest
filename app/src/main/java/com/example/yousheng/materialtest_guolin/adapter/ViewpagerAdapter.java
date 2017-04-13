71c9afa1d55a3c4b022b10620bf30d4769bdf788
Andy <1197993367@qq.com>
2017-04-13 13:04:24 +0800

    重构并加入viewpager，下一部根据不同pager里的fragment分别申请不同的网络请求，并显示

---
+++ app/src/main/java/com/example/yousheng/materialtest_guolin/adapter/ViewpagerAdapter.java
@@ -0,0 +1,10 @@
+package com.example.yousheng.materialtest_guolin.adapter;
+
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by yousheng on 17/4/13.
+ */
+
+public class ViewpagerAdapter extends FragmentPagerAdapter {
+}
